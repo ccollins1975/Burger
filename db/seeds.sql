ed2cab19ae0995161815152eefcf2f28fbc8d4bf
Chris Collins <ccollins36@icloud.com>
2020-09-10 21:20:16 -0400

    added code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (name) VALUES ('Chiken Burger');
+INSERT INTO burgers (name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (name) VALUES ('Plain Burger');
